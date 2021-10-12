48e84cdf3fab0b680e7fc1d881c84d760534f96f
xin-20010304 <1817283052@qq.com>
2021-10-12 11:59:25 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/spring_boot")
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "helloWorld Spring Boot！这是一个用Spring Boot开发的网站。";
+    }
+}
